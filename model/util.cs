0adc03c773b588ddb967189d061c4fdb34ff2b75
tonysolo <tony@soloworks.co.za>
2014-05-19 16:09:37 +0200

    comm2

--- model/util.cs
+++ model/util.cs
@@ -8,11 +8,11 @@ namespace coder.model
    static class Util
     {
 
-public static string SpaceJoin(string[] strn)
+public static string SpaceJoin(string[] str)
     {
-    for (int i = 0; i < strn.Length; i++)
-    strn[i] = strn[i].Replace(' ', '_');
-    return String.Join(" ", strn);
+    for (int i = 0; i < str.Length; i++)
+    str[i] = str[i].Replace(' ', '_');
+    return String.Join(" ", str);
     }
 
    }
